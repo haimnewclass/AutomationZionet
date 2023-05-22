e1aedd983b4ee991329ffebaf8355aec6f0984be
yaffa <boaronyaffa@gmail.com>
2023-05-22 15:48:00 +0300

    download ALL

---
+++ Automation.XNes.Lambda/LambdaFullDownload_Gemel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationZionet.Base.Driver;
+using OpenQA.Selenium;
+using AutomationZionet.Base.Scripts;
+using OpenQA.Selenium.Chrome;
+
+namespace Automation.XNes.Lambda
+{
+    public class LambdaFullDownload_Gemel: LambdaScriptBase
+    {
+        //the local path of the project
+        const string path = @"C:\Users\user\learning\xnesLearning\xnes_react\AutomationZionet\DownloadFiles";
+        public LambdaStartSelect script1;
+        public LambdaDownloadOneMonth script2;
+        public LambdaDownloadBetYears script3;
+        ScriptState ret = ScriptState.NotStarted;
+        LambdaDownloadOneMonth lambdaDownloadOneMonth;
+        FileSystemEventHandler AfileSystemEventHandler;
+        IWebDriver WebDriver;
+        //get all the gemel files
+        public LambdaFullDownload_Gemel(IWebDriver webDriver,LambdaSetting s, FileSystemEventHandler afileSystemEventHandler) :base(webDriver,s)
+        {
+            AfileSystemEventHandler = afileSystemEventHandler;
+            base.setting = s;
+            WebDriver = webDriver;
+        }
+        public override ScriptState Run()
+        {
+            ret = ScriptState.Started;
+
+            lambdaDownloadOneMonth = new LambdaDownloadOneMonth(WebDriver, base.setting,setting.lambdaConfig["Month"], setting.lambdaConfig["Year"], this.afterFileCreated);
+
+            return ret;
+        }
+
+     
+        public void afterFileCreated(object sender, FileSystemEventArgs e)
+        {
+            AfileSystemEventHandler(sender, e);
+
+
+        }
+    }
+}
---
+++ Automation.XNes.Lambda/LambdaFullDownload_Gemel_All.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomationZionet.Base.Driver;
+using OpenQA.Selenium;
+using AutomationZionet.Base.Scripts;
+using OpenQA.Selenium.Chrome;
+
+namespace Automation.XNes.Lambda
+{
+    public class LambdaFullDownload_Gemel_All: LambdaScriptBase
+    {
+        ScriptState ret = ScriptState.NotStarted;
+        //the local path of the project
+        LambdaFullDownload_Gemel lambdaFullDownload_Gemel;
+        IWebDriver WebDriver;
+        bool IsRunning = false;
+        //get all the gemel files
+        public LambdaFullDownload_Gemel_All(IWebDriver webDriver,LambdaSetting s):base(webDriver,s)
+        {
+            base.setting = s;
+            WebDriver = webDriver;
+        }
+        public override ScriptState Run()
+        {
+            ret = ScriptState.Started;
+            for (int i = 0; i < 12; i++)
+            {
+                
+                base.setting.lambdaConfig["Month"] = "01";
+                base.setting.lambdaConfig["Year"] = "2010";
+                lambdaFullDownload_Gemel = new LambdaFullDownload_Gemel(WebDriver, base.setting,this.afterFileCreated);
+                IsRunning = true;
+                lambdaFullDownload_Gemel.Run();
+                //whie IsRunning == true do nothing
+                while(IsRunning==true)
+                {
+                    //
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+
+            return ret;
+        }
+
+     
+        public void afterFileCreated(object sender, FileSystemEventArgs e)
+        {
+            IsRunning = false;
+
+
+        }
+    }
+}
